813f1620211c7a7e77d375839df5e1ab20f6c7d5
Vitaliy Vorobyov <vitalik.w555@gmail.com>
2022-12-23 17:22:24 +0300

    Fixed space invaders run script

--- collect_and_train_space_invaders.sh
+++ collect_and_train_space_invaders.sh
@@ -6,4 +6,4 @@ then
 fi
 python collect_a3c.py with "seed=1" "min_burnin=50" "max_burnin=300" "crop=30, 200" "num_steps=10" "env_id=SpaceInvadersDeterministic-v4" "load_path=model/spinv/" "save_path=data/spinv_train" "num_episodes=10000"
 python collect_a3c.py with "seed=2" "min_burnin=50" "max_burnin=300" "crop=30, 200" "num_steps=10" "env_id=SpaceInvadersDeterministic-v4" "dedup_paths=data/spinv_train" "load_path=model/spinv/" "save_path=data/spinv_eval" "num_episodes=1000"
-python run_cswm_pong.py with "dataset_path=data/spinv_train" "model_config.copy_action=True" "eval_dataset_path=data/spinv_eval" "seed=44" "epochs=100" "eval_steps=1,5,10" "learning_rate=5e-4" "model_save_path=data/cswm_spinv.pt"
+python run_cswm_space_invaders.py with "dataset_path=data/spinv_train" "model_config.copy_action=True" "eval_dataset_path=data/spinv_eval" "seed=44" "epochs=100" "eval_steps=1,5,10" "learning_rate=5e-4" "model_save_path=data/cswm_spinv.pt" "run_name=CSWM Space Invaders"
