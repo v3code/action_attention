a3c85ad98bd565ecfae123c2c87a5a9d0e6352e7
Vitaliy Vorobyov <vitalik.w555@gmail.com>
2022-12-23 16:42:39 +0300

    Improved init.sh

--- init.sh
+++ init.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-wget https://github.com/greydanus/baby-a3c/raw/master/pong-v4/model.80.tar -P ./model/
-
 pip install -r requirements.txt
 pip install gym[atari]
 pip install autorom[accept-rom-license]
